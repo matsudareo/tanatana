fe94b2f27fa008c59f1cf8a1c59b8da6c9f25a00
matudareo <mimuyao31otomo10@gmail.com>
2023-02-17 15:26:30 +0900

    k

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
                 </div>
                         <h2></h2>
                         <h1>Welcome</h1>
-                        <p class="text">利用するにはログインしてku</p>
+                        <p class="text">利用するにはログインして</p>
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
