2576074ab37fcf53502896c39d0922ff62b837c3
matudareo <mimuyao31otomo10@gmail.com>
2023-02-17 17:07:02 +0900

    head bootstrap

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -6,6 +6,12 @@
 	<title>Tanatana</title>
 	<link rel="stylesheet" type="text/css" href="{{ asset('/css/common.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('/css/media.css') }}">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<script src="./js/main.js"></script>
