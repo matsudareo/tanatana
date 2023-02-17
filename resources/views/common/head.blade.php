0a2fed6f654918338cd339e063a9c1f2d61a8819
matudareo <mimuyao31otomo10@gmail.com>
2023-02-17 17:14:50 +0900

    delete-fail

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -16,6 +16,8 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<script src="./js/main.js"></script>
+	<script src="./js/app.js"></script>
+	<script src="./js/bootstrap.js"></script>
 	<script 
   src="https://code.jquery.com/jquery-3.6.1.js"
   integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="
