da7a03dfc75f64703fc22010dcefd15285e23f56
matudareo <mimuyao31otomo10@gmail.com>
2023-02-19 18:12:49 +0900

    s

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -16,6 +16,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<script src="./js/main.js"></script>
+	<script src="./js/scripts.js"></script>
 	<script src="./js/app.js"></script>
 	<script src="./js/bootstrap.js"></script>
 	<script 
